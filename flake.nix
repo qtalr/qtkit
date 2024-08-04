7bd1a576caf30701877deedc64a775dee7148609
Jerid Francom <francojc@wfu.edu>
2024-08-03 21:10:31 -0400

    Adds flake and amends ignore

---
+++ flake.nix
@@ -0,0 +1,42 @@
+{
+  description = "Nix flake for R package development";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [ pkgs.bashInteractive ];
+          buildInputs = with pkgs; [
+            # Software
+            gh
+            git
+            pandoc
+            quarto
+            R
+            radianWrapper
+            # R packages: development
+            rPackages.devtools
+            rPackages.pkgdown
+            rPackages.testthat
+            rPackages.usethis
+            rPackages.goodpractice
+            # R packages: package
+            rPackages.Matrix
+            rPackages.dplyr
+            rPackages.ggplot2
+            rPackages.kableExtra
+            rPackages.knitr
+            rPackages.rlang
+            rPackages.rmarkdown
+            rPackages.tibble
+            rPackages.tidytext
+            rPackages.webshot2
+          ];
+        };
+      });
+}
