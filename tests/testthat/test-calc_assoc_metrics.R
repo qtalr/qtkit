2ea57cc85b3415f39b3c443406967f6985c9042d
Jerid Francom <francojc@wfu.edu>
2024-08-15 15:57:50 -0400

    adds tests for calc_assoc_metrics, create_data_dictionary, get_gutenberg_data, and get_talkbank_data functions

--- tests/testthat/test-calc_assoc_metrics.R
+++ tests/testthat/test-calc_assoc_metrics.R
@@ -1,30 +1,24 @@
-library(testthat)
 
-test_that("calc_assoc_metrics works correctly", {
+# Load the data
+data_path <- system.file("extdata", "bigrams_data.rds", package = "qtkit")
+data <- readRDS(data_path)
+
+test_that("calc_assoc_metrics handles invalid inputs", {
+  expect_error(calc_assoc_metrics(data, "nonexistent", "token_index", "type"))
+  expect_error(calc_assoc_metrics(data, "doc_index", "nonexistent", "type"))
+  expect_error(calc_assoc_metrics(data, "doc_index", "token_index", "nonexistent"))
+  expect_error(calc_assoc_metrics(data, "doc_index", "token_index", "type", "invalid"))
+})
+
+test_that("calc_assoc_metrics calculates metrics correctly", {
   data <- data.frame(
     doc_index = c(1, 1, 1, 2),
     token_index = c(1, 2, 3, 1),
     type = c("word1", "word2", "word3", "word2")
   )
-  
-  # Test 1: Check if the function returns a data frame
-  result <- calc_assoc_metrics(data, doc_index, token_index, type)
-  expect_s3_class(result, "data.frame")
-  
-  # Test 2: Check if the function returns the correct number of rows
+  result <- calc_assoc_metrics(data, "doc_index", "token_index", "type", "all", TRUE)
   expect_equal(nrow(result), 6)
-  
-  # Test 3: Check if the function returns the correct column names
-  expected_colnames <- c("y", "x", "pmi", "dice_coeff", "g_score")
-  expect_equal(colnames(result), expected_colnames)
-  
-  # Test 4: Check if the function handles invalid input correctly
-  expect_error(calc_assoc_metrics(data, invalid_column, token_index, type))
-  
-  # Test 5: Check if the function handles invalid association argument correctly
-  expect_error(calc_assoc_metrics(data, doc_index, token_index, type, association = "invalid_association"))
-
-  # TODO: complete 
-  # Test 6: Check if the function returns correct measures 
+  expect_equal(ncol(result), 8)
+  expect_equal(colnames(result), c("y", "x", "p_xy", "p_x", "p_y", "pmi", "dice_coeff", "g_score"))
 })
 
