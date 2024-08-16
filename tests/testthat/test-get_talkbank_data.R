b89bbfa3f66034c9afaf82efbb09adfd33c85d6f
Jerid Francom <francojc@wfu.edu>
2024-08-16 12:55:35 -0400

    mocks api request to talkbank

--- tests/testthat/test-get_talkbank_data.R
+++ tests/testthat/test-get_talkbank_data.R
@@ -17,28 +17,44 @@ invalid_dir <- "nonexistent_directory"
 # validate_inputs_gtd ------
 
 # valid inputs
-test_that("valid inputs", {
-  expect_silent(validate_inputs_gtd(valid_name, valid_path, valid_dir, FALSE, TRUE))
+with_mock_api({
+  test_that("valid inputs", {
+    # Test with the mocked API response
+    expect_silent(validate_inputs_gtd(
+      valid_name, valid_path, valid_dir, FALSE, TRUE))
+  })
 })
 
 # invalid corpus name
-test_that("invalid corpus name", {
-  expect_error(validate_inputs_gtd(invalid_name, valid_path, valid_dir, FALSE, TRUE))
+with_mock_api({
+  test_that("invalid corpus name", {
+    expect_error(validate_inputs_gtd(
+      invalid_name, valid_path, valid_dir, FALSE, TRUE))
+  })
 })
 
 # invalid corpus path
-test_that("invalid corpus path", {
-  expect_error(validate_inputs_gtd(valid_name, invalid_path, valid_dir, FALSE, TRUE))
+with_mock_api({
+  test_that("invalid corpus path", {
+    expect_error(validate_inputs_gtd(
+      valid_name, invalid_path, valid_dir, FALSE, TRUE))
+  })
 })
 
 # invalid boolean force
-test_that("invalid boolean force", {
-  expect_error(validate_inputs_gtd(valid_name, valid_path, valid_dir, NULL, TRUE))
+with_mock_api({
+  test_that("invalid boolean force", {
+    expect_error(validate_inputs_gtd(
+      valid_name, valid_path, valid_dir, NULL, TRUE))
+  })
 })
 
 # invalid boolean confirmed
-test_that("invalid boolean confirmed", {
-  expect_error(validate_inputs_gtd(valid_name, valid_path, valid_dir, FALSE, NULL))
+with_mock_api({
+  test_that("invalid boolean confirmed", {
+    expect_error(validate_inputs_gtd(
+      valid_name, valid_path, valid_dir, FALSE, NULL))
+  })
 })
 
 # write_to_disk function ------
@@ -60,13 +76,18 @@ mock_func_error <- function(corpus_name, corpus_path) {
   stop("mock error")
 }
 
-
 # Tests
 test_that("write_to_disk successfully writes data to disk", {
   target_dir <- file.path(temp_dir, "test_data")
   dir.create(target_dir, recursive = TRUE, showWarnings = FALSE)
   file_path <- file.path(target_dir, "test.csv")
-  result <- write_to_disk(mock_func_success, corpus_name, corpus_path, file_path, timeout = 5)
+  result <-
+    write_to_disk(
+      mock_func_success,
+      corpus_name,
+      corpus_path,
+      file_path,
+      timeout = 5)
   expect_null(result)
   expect_true(file.exists(file_path))
   expect_equal(read.csv(file_path), data.frame(a = 1, b = 2))
@@ -79,16 +100,20 @@ test_that("write_to_disk handles timeout correctly", {
   corpus_name <- "ca"
   corpus_path <- c("ca", "CABNC")
   file_path <- file.path(temp_dir, "test.csv")
-  result <- write_to_disk(getTokens, corpus_name, corpus_path, file_path, timeout = 1)
+  result <-
+    write_to_disk(getTokens, corpus_name, corpus_path, file_path, timeout = 1)
   expect_null(result)
-  expect_message(write_to_disk(getTokens, corpus_name, corpus_path, file_path, timeout = 1), regex = "timed out at 1 seconds")
+  expect_message(write_to_disk(
+    getTokens, corpus_name, corpus_path, file_path, timeout = 1),
+    regex = "timed out at 1 seconds")
 })
 
 test_that("write_to_disk does not write to disk on function error", {
   corpus_name <- "ca"
   corpus_path <- c("ca", "CABNC")
   file_path <- file.path(temp_dir, "test.csv")
-  write_to_disk(mock_func_error, corpus_name, corpus_path, file_path, timeout = 5)
+  write_to_disk(
+    mock_func_error, corpus_name, corpus_path, file_path, timeout = 5)
   expect_false(file.exists(file_path))
 })
 
@@ -99,7 +124,9 @@ test_that("write_to_disk handles empty input data correctly", {
   mock_func_empty <- function(corpus_name, corpus_path) {
     return(data.frame())
   }
-  result <- write_to_disk(mock_func_empty, corpus_name, corpus_path, file_path, timeout = 5)
+  result <-
+    write_to_disk(
+      mock_func_empty, corpus_name, corpus_path, file_path, timeout = 5)
   expect_true(is.null(result))
   expect_true(file.exists(file_path))
   expect_error(read.csv(file_path), "no lines available in input")
