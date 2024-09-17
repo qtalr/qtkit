d60e9978a09e5f2fa20904b20756f3acf9e3e145
francojc <francojc@wfu.edu>
2024-09-17 09:41:34 -0400

    formatting changes to data generation scripts

--- inst/extdata/bigrams.R
+++ inst/extdata/bigrams.R
@@ -1,10 +1,12 @@
-
 library(dplyr)
 
-bigrams_data <- tibble(
+bigrams_data <- dplyr::tibble(
   doc_index = c(1, 1, 1, 2, 2, 2, 3, 3, 3, 4, 4, 4),
-  token_index = c(1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3),
-  type = c("word1", "word2", "word3", "word2", "word3", "word4", "word3", "word4", "word5", "word4", "word5", "word6")
+  token_index = c(1, 2, 3, 1, 2, 3, 1, 2, 3, 1, 2, 3), type = c(
+    "word1", "word2",
+    "word3", "word2", "word3", "word4", "word3", "word4", "word5", "word4", "word5",
+    "word6"
+  )
 )
 
 # Save the dataset as an RDS file
